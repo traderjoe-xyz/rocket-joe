b4bbda429df5271eb1f749d3f7427993947b073a
jummy123 <jummy123@users.noreply.github.com>
2022-01-09 22:12:37 +0000

    Remove task from template

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,6 @@ require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-contract-sizer");
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
 
 module.exports = {
   solidity: "0.8.6",
