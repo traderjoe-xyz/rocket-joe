09a0776927bdcaf67dcf1bcd116e10e0cea8ede5
jummy123 <jummy123@users.noreply.github.com>
2022-01-13 15:22:49 +0000

    Rename variables; reformat

--- test/LaunchEventPhaseOne.test.js
+++ test/LaunchEventPhaseOne.test.js
@@ -1,90 +1,92 @@
 const { ethers, network } = require("hardhat");
 const { expect } = require("chai");
 const { advanceTimeAndBlock, duration } = require("./utils/time");
+const { HARDHAT_FORK_CURRENT_PARAMS } = require("./utils/hardhat")
 
-describe("Launch event contract phase one", function () {
+describe("launch event contract phase one", function () {
+
   before(async function () {
+
+    // The wallets taking part in tests.
     this.signers = await ethers.getSigners();
     this.dev = this.signers[0];
-    this.alice = this.signers[1];
-    this.bob = this.signers[2];
-    this.carol = this.signers[3];
+    this.penaltyCollector = this.signers[1];
+    this.issuer = this.signers[2];
+    this.participant = this.signers[3];
 
+    // The contracts we interact with.
+    this.wavax = await ethers.getContractAt(
+      "IWAVAX",
+      "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7"
+    );
+    this.router = await ethers.getContractAt(
+      "IJoeRouter02",
+      "0x60aE616a2155Ee3d9A68541Ba4544862310933d4"
+    );
+    this.factory = await ethers.getContractAt(
+      "IJoeFactory",
+      "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10"
+    );
+
+    // Factories for deploying our contracts.
+    this.RocketJoeTokenCF = await ethers.getContractFactory("RocketJoeToken");
+    this.RocketJoeFactoryCF = await ethers.getContractFactory(
+      "RocketJoeFactory"
+    );
+    this.LaunchEventCF = await ethers.getContractFactory("LaunchEvent");
+
+    // Fork the avalanche network to work with WAVAX.
     await network.provider.request({
       method: "hardhat_reset",
-      params: [
-        {
-          forking: {
-            jsonRpcUrl: "https://api.avax.network/ext/bc/C/rpc",
-            // blockNumber: 8465376,
-          },
-          live: false,
-          saveDeployments: true,
-          tags: ["test", "local"],
-        },
-      ],
+      params: HARDHAT_FORK_CURRENT_PARAMS,
     });
+
   });
 
   beforeEach(async function () {
-    // We want to setup
-    // The Rocket factory contract
-    // A new ERC20 for the auction
-    // rocket-joe token
-    this.WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
-    this.wavax = ethers.getContractAt("IWAVAX", this.WAVAX);
-    this.PENALTY_COLLECTOR = this.carol.address;
-    this.ROUTER = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4";
-    this.FACTORY = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
-
-    this.LaunchEventCF = await ethers.getContractFactory("LaunchEvent");
-    this.LaunchEventPrototype = await this.LaunchEventCF.deploy();
-
-    this.RocketJoeTokenCF = await ethers.getContractFactory("RocketJoeToken");
+    // Deploy the tokens used for tests.
     this.rJOE = await this.RocketJoeTokenCF.deploy();
+    // XXX: Should we replace this with a standard ERC20?
     this.AUCTOK = await this.RocketJoeTokenCF.deploy();
-    // Send rJOE to dev address
-    await this.rJOE
-      .connect(this.dev)
-      .mint(this.dev.address, "1000000000000000000000000");
 
-    await this.rJOE
-      .connect(this.dev)
-      .mint(this.bob.address, ethers.utils.parseEther("1000.0"));
-
-    // Send auction token to the dev.
-    await this.AUCTOK.connect(this.dev).mint(
-      this.dev.address,
-      "1000000000000000000000000"
-    );
-
-    this.LaunchEventCF = await ethers.getContractFactory("LaunchEvent");
-    this.RocketFactoryCF = await ethers.getContractFactory("RocketJoeFactory");
-    this.RocketFactory = await this.RocketFactoryCF.deploy(
+    // Deploy the rocket joe contracts.
+    this.LaunchEventPrototype = await this.LaunchEventCF.deploy();
+    this.RocketFactory = await this.RocketJoeFactoryCF.deploy(
       this.LaunchEventPrototype.address,
       this.rJOE.address,
-      this.WAVAX,
-      this.PENALTY_COLLECTOR,
-      this.ROUTER,
-      this.FACTORY
+      this.wavax.address,
+      this.penaltyCollector.address,
+      this.router.address,
+      this.factory.address
     );
-    this.LaunchEventPrototype.connect(this.dev).transferOwnership(
+    await this.LaunchEventPrototype.connect(this.dev).transferOwnership(
       this.RocketFactory.address
     );
 
+    // Keep a reference to the current block.
+    this.block = await ethers.provider.getBlock();
+
+    // Send the tokens used to the issuer and approve spending to the factory.
+    await this.AUCTOK.connect(this.dev).mint(
+      this.dev.address,
+      ethers.utils.parseEther("1000000")
+    ); // 1_000_000 tokens
     await this.AUCTOK.connect(this.dev).approve(
       this.RocketFactory.address,
-      "1000000000000000000000000"
+      ethers.utils.parseEther("1000000")
     );
+    await this.rJOE.connect(this.dev).mint(
+      this.participant.address,
+      ethers.utils.parseEther("1000000")
+    ); // 1_000_000 tokens
 
     // Deploy the launch event contract
-    block = await ethers.provider.getBlock();
     await this.RocketFactory.createRJLaunchEvent(
-      this.alice.address, // Issuer
-      block.timestamp + 60, // Start time (60 seconds from now)
+      this.issuer.address, // Issuer
+      this.block.timestamp + 60, // Start time (60 seconds from now)
       this.AUCTOK.address, // Address of the token being auctioned
-      100, // Floor price (100 Wei)
-      1000, // Amount of tokens for auction
+      1000000, // Amount of tokens for auction
+      1000, // Floor price (100 Wei)
       ethers.utils.parseEther("0.5"), // Max withdraw penalty
       ethers.utils.parseEther("0.4"), // Fixed withdraw penalty
       5000, // min allocation
@@ -100,174 +102,149 @@ describe("Launch event contract phase one", function () {
     );
   });
 
-  describe("Interacting with phase one", function () {
-    it("should revert if initialised twice", async function () {
-      expect(
-        this.LaunchEvent.connect(this.bob).initialize(
-          this.bob.address, // _issuer
-          block.timestamp, // _auctionStart
-          this.AUCTOK.address, // _token
-          0, // _floorPrice
-          0, // _maxWithdrawPenalty
-          0, // _fixedWithdrawPenalty
-          0, // _minAllocation
-          0, // _maxAllocation
-          0, // _userTimelock
-          0 // _issuerTimelock
-        )
-      ).to.be.revertedWith("LaunchEvent: already initialized");
-    });
+  describe("interacting with phase one", function () {
 
-    it("should revert if issuer tries to participate", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      expect(
-        this.LaunchEvent.connect(this.alice).depositAVAX({
-          value: ethers.utils.parseEther("1.0"),
-        })
-      ).to.be.revertedWith("LaunchEvent: issuer cannot participate");
-    });
-
-    it("should revert if sale has not started yet", async function () {
-      expect(
-        this.LaunchEvent.connect(this.bob).depositAVAX({
-          value: ethers.utils.parseEther("1.0"),
-        })
-      ).to.be.revertedWith("LaunchEvent: not in phase one");
-    });
-
-    it("should revert if rJOE not approved", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      expect(
-        this.LaunchEvent.connect(this.bob).depositAVAX({
-          value: ethers.utils.parseEther("1.0"),
-        })
-      ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
-    });
-
-    it("should be payable with AVAX", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE
-        .connect(this.bob)
-        .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
-      await this.LaunchEvent.connect(this.bob).depositAVAX({
-        value: ethers.utils.parseEther("1.0"),
+	describe("depositing in phase one", function () {
+      it("should revert if issuer tries to participate", async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        expect(
+          this.LaunchEvent.connect(this.issuer).depositAVAX({
+            value: ethers.utils.parseEther("1.0"),
+          })
+        ).to.be.revertedWith("LaunchEvent: issuer cannot participate");
       });
-      expect(
-        this.LaunchEvent.getUserAllocation(this.bob.address).amount
-      ).to.equal(ethers.utils.parseEther("1.0").number);
-    });
 
-    it("should revert if AVAX sent less than min allocation", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE.connect(this.bob).approve(this.LaunchEvent.address, 4999);
-      expect(
-        this.LaunchEvent.connect(this.bob).depositAVAX({ value: 4999 })
-      ).to.be.revertedWith(
-        "LaunchEvent: amount doesn't fulfill min allocation"
-      );
-    });
+      it("should revert if sale has not started yet", async function () {
+        expect(
+          this.LaunchEvent.connect(this.participant).depositAVAX({
+            value: ethers.utils.parseEther("1.0"),
+          })
+        ).to.be.revertedWith("LaunchEvent: not in phase one");
+      });
+
+      it("should revert if rJOE not approved", async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        expect(
+          this.LaunchEvent.connect(this.participant).depositAVAX({
+            value: ethers.utils.parseEther("1.0"),
+          })
+        ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+      });
+
+      it("should be payable with AVAX", async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        await this.rJOE
+          .connect(this.participant)
+          .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
+        await this.LaunchEvent.connect(this.participant).depositAVAX({
+          value: ethers.utils.parseEther("1.0"),
+        });
+        expect(
+          this.LaunchEvent.getUserAllocation(this.participant.address).amount
+        ).to.equal(ethers.utils.parseEther("1.0").number);
+      });
+
+      it("should revert if AVAX sent less than min allocation", async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        await this.rJOE.connect(this.participant).approve(this.LaunchEvent.address, 4999);
+        expect(
+          this.LaunchEvent.connect(this.participant).depositAVAX({ value: 4999 })
+        ).to.be.revertedWith(
+          "LaunchEvent: amount doesn't fulfill min allocation"
+        );
+      });
+
+      it("should revert on deposit if stopped", async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        await this.rJOE
+          .connect(this.participant)
+          .approve(this.LaunchEvent.address, 6000 * 100);
+        await this.LaunchEvent.connect(this.dev).allowEmergencyWithdraw();
+        expect(
+          this.LaunchEvent.connect(this.participant).depositAVAX({ value: 6000 })
+        ).to.be.revertedWith("LaunchEvent: stopped");
+      });
+
+      it("should revert if AVAX sent more than max allocation", async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        await this.rJOE
+          .connect(this.participant)
+          .approve(this.LaunchEvent.address, ethers.utils.parseEther("6"));
+        expect(
+          this.LaunchEvent.connect(this.participant).depositAVAX({
+            value: ethers.utils.parseEther("6"),
+          })
+        ).to.be.revertedWith("LaunchEvent: amount exceeds max allocation");
+      });
+
+      it("should burn rJOE on succesful deposit", async function () {
+        let rJOEBefore = await this.rJOE.totalSupply();
+
+        await advanceTimeAndBlock(duration.seconds(120));
+        await this.rJOE
+          .connect(this.participant)
+          .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
+
+        await this.LaunchEvent.connect(this.participant).depositAVAX({
+          value: ethers.utils.parseEther("1.0"),
+        });
+
+        expect(await this.rJOE.totalSupply()).to.be.equal(
+          rJOEBefore.sub(ethers.utils.parseEther("100.0"))
+        );
+      });
+
+	});
+
+	describe("withdrawing in phase one", function () {
+
+      beforeEach(async function () {
+        await advanceTimeAndBlock(duration.seconds(120));
+        await this.rJOE
+          .connect(this.participant)
+          .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
+        await this.LaunchEvent.connect(this.participant).depositAVAX({
+          value: ethers.utils.parseEther("1.0"),
+        });
+      });
+
+      it("should apply no fee if withdraw in first day", async function () {
+        // Test the amount received
+        const balanceBefore = await this.participant.getBalance();
+        await this.LaunchEvent.connect(this.participant).withdrawAVAX(
+          ethers.utils.parseEther("1.0")
+        );
+        expect(await this.participant.getBalance()).to.be.above(balanceBefore);
+        // Check the balance of penalty collecter.
+        expect(await this.penaltyCollector.getBalance()).to.equal(
+	    	  ethers.utils.parseEther("10000")
+	      );
+      });
+
+      it("should apply gradient fee if withdraw in second day", async function () {
+        await advanceTimeAndBlock(duration.hours(36));
+        await this.LaunchEvent.connect(this.participant).withdrawAVAX(
+          ethers.utils.parseEther("1.0")
+        );
+
+        // Check the balance of penalty collecter.
+        expect(await this.penaltyCollector.getBalance()).to.be.above(
+          ethers.utils.parseEther("10000")
+        );
+      });
+
+	  });
 
     it("should only be stopped by RJFactory owner", async function () {
       // issuer of the LaunchEvent
       await expect(
-        this.LaunchEvent.connect(this.alice).allowEmergencyWithdraw()
+        this.LaunchEvent.connect(this.issuer).allowEmergencyWithdraw()
       ).to.be.revertedWith("LaunchEvent: caller is not RocketJoeFactory owner");
 
       // any user
-      expect(
-        this.LaunchEvent.connect(this.bob).allowEmergencyWithdraw()
+      await expect(
+        this.LaunchEvent.connect(this.participant).allowEmergencyWithdraw()
       ).to.be.revertedWith("LaunchEvent: caller is not RocketJoeFactory owner");
-    });
-
-    it("should revert if stopped", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE
-        .connect(this.bob)
-        .approve(this.LaunchEvent.address, 6000 * 100);
-      await this.LaunchEvent.connect(this.dev).allowEmergencyWithdraw();
-      expect(
-        this.LaunchEvent.connect(this.bob).depositAVAX({ value: 6000 })
-      ).to.be.revertedWith("LaunchEvent: stopped");
-    });
-
-    it("should revert if trying to emergency withdraw with 0 allocation", async function () {
-      await this.LaunchEvent.connect(this.dev).allowEmergencyWithdraw();
-
-      // any user
-      expect(
-        this.LaunchEvent.connect(this.bob).emergencyWithdraw()
-      ).to.be.revertedWith(
-        "LaunchEvent: expected user to have non-zero allocation to perform emergency withdraw"
-      );
-    });
-
-    it("should revert if AVAX sent more than max allocation", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE
-        .connect(this.bob)
-        .approve(this.LaunchEvent.address, ethers.utils.parseEther("6"));
-      expect(
-        this.LaunchEvent.connect(this.bob).depositAVAX({
-          value: ethers.utils.parseEther("6"),
-        })
-      ).to.be.revertedWith("LaunchEvent: amount exceeds max allocation");
-    });
-
-    it("should burn rJOE on succesful deposit", async function () {
-      let rJOEBefore = await this.rJOE.totalSupply();
-
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE
-        .connect(this.bob)
-        .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
-
-      await this.LaunchEvent.connect(this.bob).depositAVAX({
-        value: ethers.utils.parseEther("1.0"),
-      });
-
-      expect(await this.rJOE.totalSupply()).to.be.equal(
-        rJOEBefore.sub(ethers.utils.parseEther("100.0"))
-      );
-    });
-
-    it("should apply no fee if withdraw in first day", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE
-        .connect(this.bob)
-        .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
-
-      await this.LaunchEvent.connect(this.bob).depositAVAX({
-        value: ethers.utils.parseEther("1.0"),
-      });
-
-      // Test the amount received
-      const balanceBefore = await this.bob.getBalance();
-      await this.LaunchEvent.connect(this.bob).withdrawAVAX(
-        ethers.utils.parseEther("1.0")
-      );
-      expect(await this.bob.getBalance()).to.be.above(balanceBefore);
-      // Check the balance of penalty collecter.
-      expect(await this.carol.getBalance()).to.equal("10000000000000000000000");
-    });
-
-    it("should apply gradient fee if withdraw in second day", async function () {
-      await advanceTimeAndBlock(duration.seconds(120));
-      await this.rJOE
-        .connect(this.bob)
-        .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
-
-      await this.LaunchEvent.connect(this.bob).depositAVAX({
-        value: ethers.utils.parseEther("1.0"),
-      });
-      await advanceTimeAndBlock(duration.hours(36));
-      await this.LaunchEvent.connect(this.bob).withdrawAVAX(
-        ethers.utils.parseEther("1.0")
-      );
-
-      // Check the balance of penalty collecter.
-      expect(await this.carol.getBalance()).to.be.above(
-        "10000000000000000000000"
-      );
     });
 
     it("should revert try to create pool during phase one", async function () {
@@ -279,7 +256,7 @@ describe("Launch event contract phase one", function () {
 
     it("should revert trying to send AVAX to the contract", async function () {
       await expect(
-        this.bob.sendTransaction({
+        this.participant.sendTransaction({
           to: this.LaunchEvent.address,
           value: ethers.utils.parseEther("1.0"),
         })
@@ -287,6 +264,7 @@ describe("Launch event contract phase one", function () {
         "LaunchEvent: you can't send AVAX directly to this contract"
       );
     });
+
   });
 
   after(async function () {
