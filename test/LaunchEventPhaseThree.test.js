fc15fb60e3740b90f8436e132b0baf59091409b8
jummy123 <jummy123@users.noreply.github.com>
2022-01-14 16:07:41 +0000

    Use fixture module for phase three

--- test/LaunchEventPhaseThree.test.js
+++ test/LaunchEventPhaseThree.test.js
@@ -1,103 +1,74 @@
 const { ethers, network } = require("hardhat");
 const { expect } = require("chai");
 const { advanceTimeAndBlock, duration } = require("./utils/time");
+const { HARDHAT_FORK_CURRENT_PARAMS } = require("./utils/hardhat")
+const { deployRocketFactory, createLaunchEvent } = require("./utils/contracts");
 
-describe("Launch event contract phase three", function () {
+
+describe("launch event contract phase three", function () {
   before(async function () {
+
+    // The wallets taking part in tests.
     this.signers = await ethers.getSigners();
     this.dev = this.signers[0];
-    this.alice = this.signers[1];
-    this.bob = this.signers[2];
-    this.carol = this.signers[3];
+    this.penaltyCollector = this.signers[1];
+    this.issuer = this.signers[2];
+    this.participant = this.signers[3];
 
+    this.RocketJoeTokenCF = await ethers.getContractFactory('RocketJoeToken');
+
+    // Fork the avalanche network to work with WAVAX.
     await network.provider.request({
       method: "hardhat_reset",
-      params: [
-        {
-          forking: {
-            jsonRpcUrl: "https://api.avax.network/ext/bc/C/rpc",
-            // blockNumber: 8465376,
-          },
-          live: false,
-          saveDeployments: true,
-          tags: ["test", "local"],
-        },
-      ],
+      params: HARDHAT_FORK_CURRENT_PARAMS,
     });
+
   });
 
   beforeEach(async function () {
-    this.WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
-    this.wavax = ethers.getContractAt("IWAVAX", this.WAVAX);
-    this.PENALTY_COLLECTOR = this.carol.address;
-    this.ROUTER = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4";
-    this.FACTORY = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
-
-    this.RocketJoeTokenCF = await ethers.getContractFactory("RocketJoeToken");
+    // Deploy the tokens used for tests.
     this.rJOE = await this.RocketJoeTokenCF.deploy();
+    // XXX: Should we replace this with a standard ERC20?
     this.AUCTOK = await this.RocketJoeTokenCF.deploy();
-    // Send rJOE to dev address
-    await this.rJOE
-      .connect(this.dev)
-      .mint(this.dev.address, "1000000000000000000000000");
 
-    await this.rJOE
-      .connect(this.dev)
-      .mint(this.bob.address, ethers.utils.parseEther("1000.0"));
+    // Keep a reference to the current block.
+    this.block = await ethers.provider.getBlock();
 
-    // Send auction token to the dev.
+    this.RocketFactory = await deployRocketFactory(
+      this.dev,
+      this.rJOE,
+      this.penaltyCollector);
+
+    // Send the tokens used to the issuer and approve spending to the factory.
     await this.AUCTOK.connect(this.dev).mint(
       this.dev.address,
-      "1000000000000000000000000"
-    );
-
-    this.LaunchEventCF = await ethers.getContractFactory("LaunchEvent");
-    this.RocketFactoryCF = await ethers.getContractFactory("RocketJoeFactory");
-    this.LaunchEventPrototype = await this.LaunchEventCF.deploy();
-
-    this.RocketFactory = await this.RocketFactoryCF.deploy(
-      this.LaunchEventPrototype.address,
-      this.rJOE.address,
-      this.WAVAX,
-      this.PENALTY_COLLECTOR,
-      this.ROUTER,
-      this.FACTORY
-    );
+      ethers.utils.parseEther("1000000")
+    ); // 1_000_000 tokens
     await this.AUCTOK.connect(this.dev).approve(
       this.RocketFactory.address,
-      "1000000000000000000000000"
+      ethers.utils.parseEther("1000000")
     );
+    await this.rJOE.connect(this.dev).mint(
+      this.participant.address,
+      ethers.utils.parseEther("1000000")
+    ); // 1_000_000 tokens
 
-    // Deploy the launch event contract
-    block = await ethers.provider.getBlock();
-    await this.RocketFactory.createRJLaunchEvent(
-      this.alice.address, // Issuer
-      block.timestamp + 60, // Start time (60 seconds from now)
-      this.AUCTOK.address, // Address of the token being auctioned
-      100, // Floor price (100 Wei)
-      1000, // Amount of tokens for auction
-      ethers.utils.parseEther("0.5"), // Max withdraw penalty
-      ethers.utils.parseEther("0.4"), // Fixed withdraw penalty
-      ethers.utils.parseEther("5.0"), // max allocation
-      60 * 60 * 24 * 7, // User timelock
-      60 * 60 * 24 * 8 // Issuer timelock
-    );
+    this.LaunchEvent = await createLaunchEvent(
+      this.RocketFactory,
+      this.issuer,
+      this.block,
+      this.AUCTOK);
 
-    // Get a reference to the acutal launch event contract.
-    this.LaunchEvent = await ethers.getContractAt(
-      "LaunchEvent",
-      this.RocketFactory.getRJLaunchEvent(this.AUCTOK.address)
-    );
 
     await advanceTimeAndBlock(duration.seconds(120));
     await this.rJOE
-      .connect(this.bob)
+      .connect(this.participant)
       .approve(this.LaunchEvent.address, ethers.utils.parseEther("100.0"));
-    await this.LaunchEvent.connect(this.bob).depositAVAX({
+    await this.LaunchEvent.connect(this.participant).depositAVAX({
       value: ethers.utils.parseEther("1.0"),
     });
     expect(
-      this.LaunchEvent.getUserAllocation(this.bob.address).amount
+      this.LaunchEvent.getUserAllocation(this.participant.address).amount
     ).to.equal(ethers.utils.parseEther("1.0").number);
     // increase time by 4 days
     await advanceTimeAndBlock(duration.days(4));
@@ -106,7 +77,7 @@ describe("Launch event contract phase three", function () {
   describe("Interacting with phase three", function () {
     it("should revert if try do withdraw liquidity", async function () {
       expect(
-        this.LaunchEvent.connect(this.bob).withdrawLiquidity()
+        this.LaunchEvent.connect(this.participant).withdrawLiquidity()
       ).to.be.revertedWith(
         "LaunchEvent: can't withdraw before user's timelock"
       );
@@ -114,7 +85,7 @@ describe("Launch event contract phase three", function () {
 
     it("should revert if try do withdraw WAVAX", async function () {
       expect(
-        this.LaunchEvent.connect(this.bob).withdrawAVAX(
+        this.LaunchEvent.connect(this.participant).withdrawAVAX(
           ethers.utils.parseEther("1")
         )
       ).to.be.revertedWith("LaunchEvent: unable to withdraw");
@@ -122,7 +93,7 @@ describe("Launch event contract phase three", function () {
 
     it("should revert if deposited", async function () {
       expect(
-        this.LaunchEvent.connect(this.bob).depositAVAX({
+        this.LaunchEvent.connect(this.participant).depositAVAX({
           value: ethers.utils.parseEther("1"),
         })
       ).to.be.revertedWith("LaunchEvent: not in phase one");
@@ -131,34 +102,38 @@ describe("Launch event contract phase three", function () {
     it("should revert when withdraw liquidity if pair not created", async function () {
       await advanceTimeAndBlock(duration.days(8));
       expect(
-        this.LaunchEvent.connect(this.bob).withdrawLiquidity()
+        this.LaunchEvent.connect(this.participant).withdrawLiquidity()
       ).to.be.revertedWith("LaunchEvent: pair does not exist");
     });
 
     it("should create a JoePair", async function () {
-      await this.LaunchEvent.connect(this.bob).createPair();
+      await this.LaunchEvent.connect(this.participant).createPair();
       // TODO: assert event emitted.
     });
 
     it("should revert if JoePair already created", async function () {
-      await this.LaunchEvent.connect(this.bob).createPair();
+      await this.LaunchEvent.connect(this.participant).createPair();
       expect(
-        this.LaunchEvent.connect(this.bob).createPair()
+        this.LaunchEvent.connect(this.participant).createPair()
       ).to.be.revertedWith("LaunchEvent: pair already created");
     });
 
     it("should revert if issuer tries to withdraw liquidity more than once", async function () {
-      await this.LaunchEvent.connect(this.bob).createPair();
+      await this.LaunchEvent.connect(this.participant).createPair();
 
       // increase time to allow issuer to withdraw liquidity
       await advanceTimeAndBlock(duration.days(8));
 
       // issuer withdraws liquidity
-      await this.LaunchEvent.connect(this.alice).withdrawLiquidity();
+      await this.LaunchEvent.connect(this.issuer).withdrawLiquidity();
 
       expect(
-        this.LaunchEvent.connect(this.alice).withdrawLiquidity()
+        this.LaunchEvent.connect(this.issuer).withdrawLiquidity()
       ).to.be.revertedWith("LaunchEvent: liquidity already withdrawn");
+    });
+
+    it("should report it is in the correct phase", async function () {
+      await expect(this.LaunchEvent.currentPhase() == 3);
     });
   });
 
