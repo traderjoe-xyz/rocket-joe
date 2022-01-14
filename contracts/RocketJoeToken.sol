44a80748bda94d87618df7f1de9108bde72af910
Louis <louismemyself@gmail.com>
2022-01-14 18:07:23 +0100

    non transferable

--- contracts/RocketJoeToken.sol
+++ contracts/RocketJoeToken.sol
@@ -4,11 +4,23 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IRocketJoeFactory.sol";
 
 /// @title Rocket Joe Token, rJOE
 /// @author traderjoexyz
 /// @notice Infinite supply, but burned to join IDO
 contract RocketJoeToken is ERC20("RocketJoeToken", "rJOE"), Ownable {
+    IRocketJoeFactory public rocketJoeFactory;
+    bool private initialized;
+
+    /// @notice Initialise the rocketJoeFactory address
+    function initialize() external {
+        require(!initialized, "RocketJoeToken: Already initialized");
+
+        rocketJoeFactory = IRocketJoeFactory(msg.sender);
+        initialized = true;
+    }
+
     /// @dev Creates `_amount` token to `_to`. Must only be called by the owner (RocketJoeStaking)
     function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
@@ -17,5 +29,22 @@ contract RocketJoeToken is ERC20("RocketJoeToken", "rJOE"), Ownable {
     /// @dev Destroys `_amount` tokens from `msg.sender`
     function burn(uint256 _amount) external {
         _burn(msg.sender, _amount);
+    }
+
+    /// @dev Hook that is called before any transfer of tokens. This includes
+    /// minting and burning
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        require(
+            from == address(0) ||
+            to == address(0) ||
+            from == owner() ||
+            rocketJoeFactory.isLaunchEvent(from) ||
+            rocketJoeFactory.isLaunchEvent(to),
+            "rJOE: can't send token"
+        );
     }
 }
---
+++ contracts/mocks/ERC20Token.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title Joe Token, JOE
+/// @author traderjoexyz
+/// @dev ONLY FOR TESTS
+contract ERC20Token is ERC20("Token", "token"), Ownable {
+    /// @dev Mint _amount to _to. Callable only by owner
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    /// @dev Destroys `_amount` tokens from `msg.sender`
+    function burn(uint256 _amount) external {
+        _burn(msg.sender, _amount);
+    }
+}
