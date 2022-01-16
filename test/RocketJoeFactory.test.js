f16e1a098f6fc6569459802ef952b883ad942802
jummy123 <jummy123@users.noreply.github.com>
2022-01-16 20:45:33 +0000

    Remove un-needed describe/fixture

--- test/RocketJoeFactory.test.js
+++ test/RocketJoeFactory.test.js
@@ -10,7 +10,6 @@ describe("rocket factory test", function () {
     this.dev = this.signers[0];
     this.penaltyCollector = this.signers[1];
     this.issuer = this.signers[2];
-    this.participant = this.signers[3];
 
     this.RocketJoeTokenCF = await ethers.getContractFactory("RocketJoeToken");
 
@@ -33,112 +32,110 @@ describe("rocket factory test", function () {
     );
   });
 
-  describe("setting variables", function () {
-    it("should set rJoe token address", async function () {
-      await expect(
-        this.RocketFactory.connect(this.issuer).setRJoe(this.signers[9].address)
-      ).to.be.revertedWith("Ownable: caller is not the owner");
-      await this.RocketFactory.connect(this.dev).setRJoe(
-        this.signers[8].address
-      );
-      expect(await this.RocketFactory.rJoe()).to.equal(this.signers[8].address);
-    });
+  it("should set rJoe token address", async function () {
+    await expect(
+      this.RocketFactory.connect(this.issuer).setRJoe(this.signers[9].address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+    await this.RocketFactory.connect(this.dev).setRJoe(
+      this.signers[8].address
+    );
+    expect(await this.RocketFactory.rJoe()).to.equal(this.signers[8].address);
+  });
 
-    it("should set penalty collector token address", async function () {
-      await expect(
-        this.RocketFactory.connect(this.issuer).setPenaltyCollector(
-          this.signers[9].address
-        )
-      ).to.be.revertedWith("Ownable: caller is not the owner");
-      await this.RocketFactory.connect(this.dev).setPenaltyCollector(
-        this.signers[8].address
-      );
-      expect(await this.RocketFactory.penaltyCollector()).to.equal(
-        this.signers[8].address
-      );
-    });
+  it("should set penalty collector token address", async function () {
+    await expect(
+      this.RocketFactory.connect(this.issuer).setPenaltyCollector(
+        this.signers[9].address
+      )
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+    await this.RocketFactory.connect(this.dev).setPenaltyCollector(
+      this.signers[8].address
+    );
+    expect(await this.RocketFactory.penaltyCollector()).to.equal(
+      this.signers[8].address
+    );
+  });
 
-    it("should set router token address", async function () {
-      await expect(
-        this.RocketFactory.connect(this.issuer).setRouter(
-          this.signers[9].address
-        )
-      ).to.be.revertedWith("Ownable: caller is not the owner");
-      await this.RocketFactory.connect(this.dev).setRouter(
-        this.signers[8].address
-      );
-      expect(await this.RocketFactory.router()).to.equal(
-        this.signers[8].address
-      );
-    });
+  it("should set router token address", async function () {
+    await expect(
+      this.RocketFactory.connect(this.issuer).setRouter(
+        this.signers[9].address
+      )
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+    await this.RocketFactory.connect(this.dev).setRouter(
+      this.signers[8].address
+    );
+    expect(await this.RocketFactory.router()).to.equal(
+      this.signers[8].address
+    );
+  });
 
-    it("should set factory address", async function () {
-      await expect(
-        this.RocketFactory.connect(this.issuer).setFactory(
-          this.signers[9].address
-        )
-      ).to.be.revertedWith("Ownable: caller is not the owner");
-      await this.RocketFactory.connect(this.dev).setFactory(
-        this.signers[8].address
-      );
-      expect(await this.RocketFactory.factory()).to.equal(
-        this.signers[8].address
-      );
-    });
+  it("should set factory address", async function () {
+    await expect(
+      this.RocketFactory.connect(this.issuer).setFactory(
+        this.signers[9].address
+      )
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+    await this.RocketFactory.connect(this.dev).setFactory(
+      this.signers[8].address
+    );
+    expect(await this.RocketFactory.factory()).to.equal(
+      this.signers[8].address
+    );
+  });
 
-    it("should set rJoe per avax address", async function () {
-      await expect(
-        this.RocketFactory.connect(this.issuer).setRJoePerAvax(
-          ethers.utils.parseEther("1")
-        )
-      ).to.be.revertedWith("Ownable: caller is not the owner");
-      await this.RocketFactory.connect(this.dev).setRJoePerAvax(
-        ethers.utils.parseEther("31337")
-      );
-      expect(await this.RocketFactory.rJoePerAvax()).to.equal(
-        ethers.utils.parseEther("31337")
-      );
-    });
+  it("should set rJoe per avax address", async function () {
+    await expect(
+      this.RocketFactory.connect(this.issuer).setRJoePerAvax(
+        ethers.utils.parseEther("1")
+      )
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+    await this.RocketFactory.connect(this.dev).setRJoePerAvax(
+      ethers.utils.parseEther("31337")
+    );
+    expect(await this.RocketFactory.rJoePerAvax()).to.equal(
+      ethers.utils.parseEther("31337")
+    );
+  });
 
-    it("should increment the number of launch events", async function () {
-      expect(await this.RocketFactory.numLaunchEvents()).to.equal(0);
-      const token1 = await this.RocketJoeTokenCF.deploy();
-      await token1
-        .connect(this.dev)
-        .mint(this.dev.address, ethers.utils.parseEther("1000000"));
-      await token1
-        .connect(this.dev)
-        .approve(
-          this.RocketFactory.address,
-          ethers.utils.parseEther("1000000")
-        );
-      await createLaunchEvent(
-        this.RocketFactory,
-        this.issuer,
-        await ethers.provider.getBlock(),
-        token1
+  it("should increment the number of launch events", async function () {
+    expect(await this.RocketFactory.numLaunchEvents()).to.equal(0);
+    const token1 = await this.RocketJoeTokenCF.deploy();
+    await token1
+      .connect(this.dev)
+      .mint(this.dev.address, ethers.utils.parseEther("1000000"));
+    await token1
+      .connect(this.dev)
+      .approve(
+        this.RocketFactory.address,
+        ethers.utils.parseEther("1000000")
       );
-      expect(await this.RocketFactory.numLaunchEvents()).to.equal(1);
+    await createLaunchEvent(
+      this.RocketFactory,
+      this.issuer,
+      await ethers.provider.getBlock(),
+      token1
+    );
+    expect(await this.RocketFactory.numLaunchEvents()).to.equal(1);
 
-      const token2 = await this.RocketJoeTokenCF.deploy();
+    const token2 = await this.RocketJoeTokenCF.deploy();
 
-      await token2
-        .connect(this.dev)
-        .mint(this.dev.address, ethers.utils.parseEther("1000000"));
-      await token2
-        .connect(this.dev)
-        .approve(
-          this.RocketFactory.address,
-          ethers.utils.parseEther("1000000")
-        );
-      await createLaunchEvent(
-        this.RocketFactory,
-        this.issuer,
-        await ethers.provider.getBlock(),
-        token2
+    await token2
+      .connect(this.dev)
+      .mint(this.dev.address, ethers.utils.parseEther("1000000"));
+    await token2
+      .connect(this.dev)
+      .approve(
+        this.RocketFactory.address,
+        ethers.utils.parseEther("1000000")
       );
-      expect(await this.RocketFactory.numLaunchEvents()).to.equal(2);
-    });
+    await createLaunchEvent(
+      this.RocketFactory,
+      this.issuer,
+      await ethers.provider.getBlock(),
+      token2
+    );
+    expect(await this.RocketFactory.numLaunchEvents()).to.equal(2);
   });
 
   after(async function () {
