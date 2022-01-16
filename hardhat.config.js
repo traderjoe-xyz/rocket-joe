c390c1c55d79c2145c335006af76394d04265d67
jummy123 <jummy123@users.noreply.github.com>
2022-01-16 23:33:47 +0000

    Remove faucet task

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,6 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-contract-sizer");
 require("solidity-coverage");
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
 
 module.exports = {
   solidity: "0.8.6",
