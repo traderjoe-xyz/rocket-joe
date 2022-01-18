305f1a8a11425f03b20c07d82ea06804409edb7f
jummy123 <jummy123@users.noreply.github.com>
2022-01-18 10:32:53 +0000

    s/tokenBalance/tokenReserve/g

--- contracts/LaunchEvent.sol
+++ contracts/LaunchEvent.sol
@@ -426,7 +426,7 @@ contract LaunchEvent is Ownable {
                 uint256 amount = tokenReserve;
                 tokenReserve = 0;
                 token.transfer(issuer, amount);
-                tokenBalance -= amount;
+                tokenReserve -= amount;
             }
         } else {
             pair.transfer(msg.sender, balance);
