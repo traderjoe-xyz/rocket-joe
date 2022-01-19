6036bfd94aa994a9f03e3674a38ec906c514e314
Michael George <mdgeorge@cs.cornell.edu>
2022-01-19 16:20:19 -0500

    removed some of the template cruft

---
+++ certora/harness/RocketJoeStakingHarness.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+// This is the contract that is actually verified; it may contain some helper
+// methods for the spec to access internal state, or may override some of the
+// more complex methods in the original contract.
+
+import "../munged/RocketJoeStaking.sol";
+
+// TODO: change the name and supercontract and add any necessary harnessing
+contract RocketJoeStakingHarness is RocketJoeStaking {
+
+}
+
--- certora/scripts/sanity.sh
+++ certora/scripts/sanity.sh
@@ -1,7 +1,7 @@
 
 
 certoraRun \
-    certora/munged/LaunchEvent.sol \
+    certora/harness/LaunchEventHarness.sol \
     certora/helpers/DummyERC20A.sol    \
     certora/helpers/DummyERC20B.sol    \
     certora/munged/RocketJoeFactory.sol    \
@@ -11,27 +11,27 @@ certoraRun \
     certora/munged/traderjoe/JoePair.sol \
     certora/munged/traderjoe/libraries/JoeLibrary.sol \
     certora/munged/traderjoe/JoeFactory.sol \
-    --link LaunchEvent:pair=JoePair \
-    LaunchEvent:factory=JoeFactory \
-    LaunchEvent:router=JoeRouter02 \
-    LaunchEvent:rJoe=RocketJoeToken \
-    LaunchEvent:rocketJoeFactory=RocketJoeFactory \
-    LaunchEvent:WAVAX=DummyWeth \
-           RocketJoeFactory:eventImplementation=LaunchEvent  RocketJoeFactory:rJoe=RocketJoeToken \
-    --verify LaunchEvent:certora/spec/sanity.spec \
-    --solc_map JoeLibrary=solc6.12,LaunchEvent=solc8.6,DummyERC20A=solc8.6,DummyERC20B=solc8.6,RocketJoeFactory=solc8.6,RocketJoeToken=solc8.6,DummyWeth=solc8.6,JoeRouter02=solc6.12,JoePair=solc6.12,JoeFactory=solc6.12 \
+    --link LaunchEventHarness:pair=JoePair \
+    LaunchEventHarness:factory=JoeFactory \
+    LaunchEventHarness:router=JoeRouter02 \
+    LaunchEventHarness:rJoe=RocketJoeToken \
+    LaunchEventHarness:rocketJoeFactory=RocketJoeFactory \
+    LaunchEventHarness:WAVAX=DummyWeth \
+           RocketJoeFactory:eventImplementation=LaunchEventHarness  RocketJoeFactory:rJoe=RocketJoeToken \
+    --verify LaunchEventHarness:certora/spec/sanity.spec \
+    --solc_map JoeLibrary=solc6.12,LaunchEventHarness=solc8.6,DummyERC20A=solc8.6,DummyERC20B=solc8.6,RocketJoeFactory=solc8.6,RocketJoeToken=solc8.6,DummyWeth=solc8.6,JoeRouter02=solc6.12,JoePair=solc6.12,JoeFactory=solc6.12 \
     --staging \
     --msg "sanity launch" \
     # --staging \=
     $*
 
 certoraRun \
-    certora/munged/RocketJoeStaking.sol \
+    certora/harness/RocketJoeStakingHarness.sol \
     certora/helpers/DummyERC20A.sol    \
     certora/helpers/DummyERC20B.sol    \
     certora/munged/RocketJoeToken.sol  \
-    --link RocketJoeStaking:rJoe=RocketJoeToken \
-    --verify RocketJoeStaking:certora/spec/sanity.spec \
+    --link RocketJoeStakingHarness:rJoe=RocketJoeToken \
+    --verify RocketJoeStakingHarness:certora/spec/sanity.spec \
     --solc solc8.6                      \
     --staging \
     --msg "sanity staking" \
