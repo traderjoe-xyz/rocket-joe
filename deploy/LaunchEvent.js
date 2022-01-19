90bfc98a6a24b25d0fa2ff3499602578d0e62f37
jummy123 <jummy123@users.noreply.github.com>
2022-01-19 11:30:17 +0000

    Run prettier on deploy files

--- deploy/LaunchEvent.js
+++ deploy/LaunchEvent.js
@@ -1,10 +1,10 @@
-module.exports = async ({getNamedAccounts, deployments}) => {
-  const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
-  await deploy('LaunchEvent', {
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  await deploy("LaunchEvent", {
     from: deployer,
     args: [],
     log: true,
   });
 };
-module.exports.tags = ['LaunchEvent'];
+module.exports.tags = ["LaunchEvent"];
--- deploy/RocketJoeToken.js
+++ deploy/RocketJoeToken.js
@@ -1,6 +1,6 @@
-module.exports = async function ({ getNamedAccounts, deployments}) {
-  const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
   await deploy("RocketJoeToken", {
     from: deployer,
     args: [],
