3b3b31d73dc18a8968f00768db55b847e74d8e26
jummy123 <jummy123@users.noreply.github.com>
2022-01-19 11:43:42 +0000

    Load rinkeby private key/alchemy project ID from environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,8 +13,8 @@ module.exports = {
     hardhat: {
     },
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
-      accounts: []
+      url: `https://eth-rinkeby.alchemyapi.io/v2/{$process.env.ALCHEMY_PROJECT_ID || ''}`,
+      accounts: (process.env.RINKEBY_PRIVATE_KEY ? [process.env.RINKEBY_PRIVATE_KEY] : [])
     },
   },
   settings: {
