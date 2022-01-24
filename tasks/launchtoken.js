215dbdfc0ee4fbd76414c074fb421799dfd9e27c
Lint Action <lint-action@samuelmeuli.com>
2022-01-24 00:47:50 +0000

    Fix code style issues with Prettier

--- tasks/launchtoken.js
+++ tasks/launchtoken.js
@@ -1,15 +1,18 @@
 task("launch", "Deploy's an ERC20 token and creates a launch event")
-  .addParam('issuer', 'The launch events issuer address')
-  .addParam('start', 'Inix timestamp for auction start time')
-  .addParam('amount', 'The amount of token being aunctioned')
-  .addParam('incentives', 'Percentage of tokens as incentive')
-  .addParam('floor', 'The token floor price')
-  .addParam('maxpenalty', 'The maximum of the variable withdraw penalty in phase 1')
-  .addParam('fixedpenalty', 'The fixed withdraw penalty in phase 2')
-  .addParam('maxallocation', 'The maximum allocation per ethereum account')
-  .addParam('usertimelock', 'The users withdraw timelock')
-  .addParam('issuertimelock', 'The issuers withdraw timelock')
-  .addParam('rocketfactoryaddress', 'The address of rocket joe factory')
+  .addParam("issuer", "The launch events issuer address")
+  .addParam("start", "Inix timestamp for auction start time")
+  .addParam("amount", "The amount of token being aunctioned")
+  .addParam("incentives", "Percentage of tokens as incentive")
+  .addParam("floor", "The token floor price")
+  .addParam(
+    "maxpenalty",
+    "The maximum of the variable withdraw penalty in phase 1"
+  )
+  .addParam("fixedpenalty", "The fixed withdraw penalty in phase 2")
+  .addParam("maxallocation", "The maximum allocation per ethereum account")
+  .addParam("usertimelock", "The users withdraw timelock")
+  .addParam("issuertimelock", "The issuers withdraw timelock")
+  .addParam("rocketfactoryaddress", "The address of rocket joe factory")
   .setAction(async (taskArgs) => {
     // Get deployer account.
     const accounts = await hre.ethers.getSigners();
@@ -29,7 +32,10 @@ task("launch", "Deploy's an ERC20 token and creates a launch event")
     console.log(`Approved ${taskArgs.rocketfactoryaddress} ${token.address}`);
 
     // Create launch event.
-    const rocketFactory = await hre.ethers.getContractAt("RocketJoeFactory", taskArgs.rocketfactoryaddress);
+    const rocketFactory = await hre.ethers.getContractAt(
+      "RocketJoeFactory",
+      taskArgs.rocketfactoryaddress
+    );
     await rocketFactory.createRJLaunchEvent(
       taskArgs.issuer,
       taskArgs.start,
@@ -41,7 +47,7 @@ task("launch", "Deploy's an ERC20 token and creates a launch event")
       taskArgs.fixedpenalty,
       taskArgs.maxallocation,
       taskArgs.usertimelock,
-      taskArgs.issuertimelock,
+      taskArgs.issuertimelock
     );
     console.log(`Created launch event for ${token.address}`);
   });
