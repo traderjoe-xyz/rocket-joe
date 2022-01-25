b29a64549868def29f4e5c143c8ec2ff94db4727
0xnivek <0xnivek@gmail.com>
2022-01-25 09:34:47 -0500

    hardcode rocketjoe factory address for rinkeby

--- deploy/LaunchEventLens.js
+++ deploy/LaunchEventLens.js
@@ -1,9 +1,21 @@
+const RINKEBY_ROCKET_JOE_FACTORY_ADDRESS =
+  "0xE2a1631268cFfE307bb1Ed002dA43dA78EB8b8B6";
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const rocketJoeFactoryAddress = (await deployments.get("RocketJoeFactory"))
-    .address;
+  const chainId = await getChainId();
+
+  let rocketJoeFactoryAddress;
+  if (chainId === 4) {
+    // rinkeby contract addresses
+    rocketJoeFactoryAddress = RINKEBY_ROCKET_JOE_FACTORY_ADDRESS;
+  } else if (chainId == 43114 || chainId == 31337) {
+    // avalanche mainnet or hardhat network addresses
+    rocketJoeFactoryAddress = (await deployments.get("RocketJoeFactory"))
+      .address;
+  }
 
   await deploy("LaunchEventLens", {
     from: deployer,
@@ -13,4 +25,5 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 };
 
 module.exports.tags = ["LaunchEventLens"];
-module.exports.dependencies = ["RocketJoeFactory"];
+// TODO: Uncomment this once we are ready to deploy to mainnet
+// module.exports.dependencies = ["RocketJoeFactory"];
