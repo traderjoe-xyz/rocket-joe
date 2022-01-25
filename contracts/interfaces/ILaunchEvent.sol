bdab6d2a0a6dd113ff6e2c2b75c387ddca11dae1
0xnivek <0xnivek@gmail.com>
2022-01-24 19:23:53 -0500

    Add RocketJoeLens

--- contracts/interfaces/ILaunchEvent.sol
+++ contracts/interfaces/ILaunchEvent.sol
@@ -2,7 +2,18 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import "./IJoePair.sol";
+
 interface ILaunchEvent {
+    struct UserInfo {
+        uint256 allocation;
+        uint256 balance;
+        bool hasWithdrawnPair;
+        bool hasWithdrawnIncentives;
+    }
+
     function initialize(
         address _issuer,
         uint256 _phaseOne,
@@ -15,4 +26,32 @@ interface ILaunchEvent {
         uint256 _userTimelock,
         uint256 _issuerTimelock
     ) external;
+
+    function auctionStart() external view returns (uint256);
+
+    function PHASE_ONE_DURATION() external view returns (uint256);
+
+    function PHASE_ONE_NO_FEE_DURATION() external view returns (uint256);
+
+    function PHASE_TWO_DURATION() external view returns (uint256);
+
+    function tokenIncentivesPercent() external view returns (uint256);
+
+    function floorPrice() external view returns (uint256);
+
+    function userTimelock() external view returns (uint256);
+
+    function issuerTimelock() external view returns (uint256);
+
+    function maxWithdrawPenalty() external view returns (uint256);
+
+    function fixedWithdrawPenalty() external view returns (uint256);
+
+    function rJoePerAvax() external view returns (uint256);
+
+    function token() external view returns (IERC20Metadata);
+
+    function pair() external view returns (IJoePair);
+
+    function getUserInfo(address) external view returns (UserInfo memory);
 }
