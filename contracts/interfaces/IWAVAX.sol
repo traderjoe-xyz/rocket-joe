ba15902bebd14864b17cb136ba622ef0f2ead52c
jummy123 <jummy123@users.noreply.github.com>
2022-01-25 21:51:04 +0000

    IWAVAX.balanceOf is a view

--- contracts/interfaces/IWAVAX.sol
+++ contracts/interfaces/IWAVAX.sol
@@ -9,7 +9,7 @@ interface IWAVAX {
 
     function withdraw(uint256) external;
 
-    function balanceOf(address account) external returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
 
     function approve(address spender, uint256 amount) external returns (bool);
 }
