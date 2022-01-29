ed6e0f819b69f8304090f358290cab232175a4c7
0xnivek <0xnivek@gmail.com>
2022-01-28 19:48:29 -0500

    Add handling for avaxAllocated == 0 in getIncentives

--- contracts/LaunchEvent.sol
+++ contracts/LaunchEvent.sol
@@ -599,6 +599,7 @@ contract LaunchEvent {
             if (address(pair) == address(0)) return tokenIncentiveIssuerRefund;
             return tokenIncentiveIssuerRefund + tokenReserve;
         } else {
+            if (avaxAllocated == 0) return 0;
             return (user.balance * tokenIncentivesForUsers) / avaxAllocated;
         }
     }
