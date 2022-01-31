e2f5322673ca5c9c0b3dd492fa1580df83f91ba9
LouisMeMyself <louismemyself@gmail.com>
2022-01-31 15:26:44 +0100

    reetrancy create RJLaunch

--- contracts/RocketJoeFactory.sol
+++ contracts/RocketJoeFactory.sol
@@ -153,6 +153,10 @@ contract RocketJoeFactory is
 
         address launchEvent = Clones.clone(eventImplementation);
 
+        getRJLaunchEvent[_token] = launchEvent;
+        isRJLaunchEvent[launchEvent] = true;
+        allRJLaunchEvents.push(launchEvent);
+
         // msg.sender needs to approve RocketJoeFactory
         IERC20(_token).safeTransferFrom(
             msg.sender,
@@ -174,10 +178,6 @@ contract RocketJoeFactory is
             _userTimelock,
             _issuerTimelock
         );
-
-        getRJLaunchEvent[_token] = launchEvent;
-        isRJLaunchEvent[launchEvent] = true;
-        allRJLaunchEvents.push(launchEvent);
 
         _emitLaunchedEvent(launchEvent, _issuer, _token, _phaseOneStartTime);
 
