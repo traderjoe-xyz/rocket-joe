a3e602b78ef2631014fdfdb13ed3b0baf6e95bd8
Aleksander Kryukov <firealexkryukov@gmail.com>
2022-02-09 19:57:37 +0000

    sh scripts for LE

---
+++ certora/scripts/verifyLEHighLevel.sh
@@ -0,0 +1,27 @@
+
+
+certoraRun \
+    certora/harness/LaunchEventHarness.sol \
+    certora/harness/Owner.sol \
+    certora/helpers/DummyERC20A.sol    \
+    certora/helpers/DummyERC20B.sol    \
+    certora/munged/RocketJoeFactory.sol    \
+    certora/munged/RocketJoeToken.sol      \
+    certora/helpers/DummyWeth.sol          \
+    certora/munged/traderjoe/JoeRouter02.sol \
+    certora/munged/traderjoe/JoePair.sol \
+    certora/munged/traderjoe/libraries/JoeLibrary.sol \
+    certora/munged/traderjoe/JoeFactory.sol \
+    --link LaunchEventHarness:pair=JoePair \
+    LaunchEventHarness:factory=JoeFactory \
+    LaunchEventHarness:router=JoeRouter02 \
+    LaunchEventHarness:rJoe=RocketJoeToken \
+    LaunchEventHarness:rocketJoeFactory=RocketJoeFactory \
+    LaunchEventHarness:WAVAX=DummyWeth \
+            RocketJoeFactory:eventImplementation=LaunchEventHarness  RocketJoeFactory:rJoe=RocketJoeToken \
+    --verify LaunchEventHarness:certora/spec/LEHighLevel.spec \
+    --solc_map Owner=solc8.6,JoeLibrary=solc6.12,LaunchEventHarness=solc8.6,DummyERC20A=solc8.6,DummyERC20B=solc8.6,RocketJoeFactory=solc8.6,RocketJoeToken=solc8.6,DummyWeth=solc8.6,JoeRouter02=solc6.12,JoePair=solc6.12,JoeFactory=solc6.12 \
+    --optimistic_loop \
+    --staging \
+    --rule hl_withdrawLateMorePenalty \
+    --msg "$1"
